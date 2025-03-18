c01eaf5c461582015383ab42e047ebeef6c415a1
MtKana <kana95372@gmail.com>
2025-03-18 13:34:10 +0900

    Remove obsolete DAQ and preprocessing scripts for BMI and oddball modules

--- monetaryIncentiveDelay/daq_check.m
+++
@@ -1,5 +0,0 @@
-d = daq('ni');
-ch = addinput(d, 'Dev2', 'ai3', 'Voltage');
-
-data = read(d, 1);
-fprintf('AI3 Voltage: %.3f V\n', data.Dev2_ai3);
--- monetaryIncentiveDelay/initialize_key.m
+++
@@ -1,6 +0,0 @@
-%% initialize preprocess for BMI
-cd('C:\Users\UshibaLab\01-individuals\matsuyanagi\monetaryIncentiveDelay');
-
-user_data = struct;
-
-user_data.count = 0;
