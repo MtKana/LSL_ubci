244bffbd71121c7f65a44fe2887fb627155b2bb8
MtKana <kana95372@gmail.com>
2025-03-18 13:34:20 +0900

    Fix order of processing in postprocess_goNoGo function

--- goNoGo/postprocess_goNoGo.m
+++ goNoGo/postprocess_goNoGo.m
@@ -8,8 +8,8 @@ function [results, user_data] = postprocess_goNoGo(stream_data_struct_array, use
 %         results(1).time_stamps = stream_data_struct_array(1).time_stamps;
 
         %% EGI NetAmp 0 (1)
-        user_data.LSL_DAQ_goNoGo = user_data.LSL_DAQ_goNoGo.process_daq(stream_data_struct_array(1).time_series');%[samp ch]
         user_data.LSL_DAQ_goNoGo = user_data.LSL_DAQ_goNoGo.time_keeper;    
+        user_data.LSL_DAQ_goNoGo = user_data.LSL_DAQ_goNoGo.process_daq(stream_data_struct_array(1).time_series');%[samp ch]
         user_data.LSL_DAQ_goNoGo = user_data.LSL_DAQ_goNoGo.show_protocol;
     end
 end
